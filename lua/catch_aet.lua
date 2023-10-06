76b16976ef24fa845ad417095d137a81cf58a017
Hasan-aga <careerforma@gmail.com>
2023-10-06 19:01:56 +0300

    get aet

---
+++ lua/catch_aet.lua
@@ -0,0 +1,26 @@
+function ReceivedInstanceFilter(dicom, origin, info)
+    print("Lua script triggered")
+    
+    local path = "/lua/"  -- Change this to your desired path
+    
+    if origin.RemoteAet ~= nil then
+        local filename = path .. origin.RemoteAet .. ".txt"
+        
+        -- Check if the file already exists
+        local f = io.open(filename, "r")
+        if f then
+            -- File exists, close it and do nothing
+            f:close()
+        else
+            -- File doesn't exist, create it and write the AETitle
+            local f = io.open(filename, "w")
+            if f then
+                f:write(origin.RemoteAet)
+                f:close()
+                print("File created for AETitle:", origin.RemoteAet)
+            end
+        end
+    end
+
+    return true
+end
